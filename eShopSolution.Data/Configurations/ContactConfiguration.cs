5519ba9126781b17279f2e9b772fd94ce4125394
Lê Huy <lehuylxag1995@gmail.com>
2020-02-23 13:17:50 +0700

    # Add Model # Config Fluent API for class model # Add Migration and Update database

---
+++ eShopSolution.Data/Configurations/ContactConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using eShopSolution.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Email)
+                .IsUnicode()
+                .IsRequired()
+                .HasMaxLength(200);
+            builder.Property(x => x.Message).IsRequired();
+            builder.Property(x => x.PhoneNumber)
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+            
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class Cart
+    {
+        public int Id { set; get; }
+        
+        public int Quantity { set; get; }
+        public decimal Price { set; get; }
+        public Guid UserId { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        //1 giỏ hàng có nhiều sản phẩm
+        public int ProductId { set; get; }
+        public Product Product { get; set; }
+    }
+}
